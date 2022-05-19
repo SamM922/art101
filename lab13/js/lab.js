98ed7d424d108488b852c5a1211e231e3d5dae51
SamM922 <samuel.maturo@gmail.com>
2022-05-18 22:38:14 -0700

    Lab 13

---
+++ lab13/js/lab.js
@@ -0,0 +1,30 @@
+// Author: Samuel Maturo
+// Created: May 18th, 2022
+
+//Create a funcion to sort Fizz Buzz and Boom
+function fizzBuzzBoom(max) {
+  //Loop through all the numbers from 1 to the specified max
+  for (var i = 1; i <= max; i++) {
+    //Catch the cases that match the contidion for Fizz Buzz and Boom and append the results
+    if (i%3 == 0 && i%5 == 0 && i%7 == 0) {
+      $("#output").append("<p>" + i + " FizzBuzzBoom!</p>");
+    } else if (i%5 == 0 && i%7 == 0) {
+      $("#output").append("<p>" + i + " BuzzBoom!</p>");
+    } else if (i%3 == 0 && i%7 == 0) {
+      $("#output").append("<p>" + i + " FizzBoom!</p>");
+    } else if (i%3 == 0 && i%5 == 0) {
+      $("#output").append("<p>" + i + " FizzBuzz!</p>");
+    } else if (i%3 == 0) {
+      $("#output").append("<p>" + i + " Fizz!</p>");
+    } else if (i%5 == 0) {
+      $("#output").append("<p>" + i + " Buzz!</p>");
+    } else if (i%7 == 0) {
+      $("#output").append("<p>" + i + " Boom!</p>");
+    } else {
+      $("#output").append("<p>" + i + "</p>");
+      console.log("Hey");
+    }
+  }
+}
+//Call the funciton with 200 as the max
+fizzBuzzBoom(200);
